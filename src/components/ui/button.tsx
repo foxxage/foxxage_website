f1a032cd4006f7e0c7ec78121f5c96ef7b890cd9
Foxxage <manage.foxxage@gmail.com>
2025-09-28 22:22:47 +0000

    add fade in up hover effect on the bottons

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -44,7 +44,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
     const Comp = asChild ? Slot : "button"
     return (
       <Comp
-        className={cn(buttonVariants({ variant, size, className }), "animated-outline")}
+        className={cn(buttonVariants({ variant, size, className }), "animated-outline hover:animate-fade-in-up")}
         ref={ref}
         {...props}
       />
