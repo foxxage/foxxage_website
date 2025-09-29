93065f445480451a159c85bce4a857a1a47158d5
Foxxage <manage.foxxage@gmail.com>
2025-09-29 09:37:22 +0000

    Make it bold (_for element h1_)

--- src/app/terms-conditions/page.tsx
+++ src/app/terms-conditions/page.tsx
@@ -8,7 +8,7 @@ export default function TermsAndConditionsPage() {
       <Header />
       <main className="flex-grow container mx-auto px-4 sm:px-6 lg:px-8 py-16">
         <div className="max-w-4xl mx-auto prose dark:prose-invert">
-          <h1>Terms & Conditions</h1>
+          <h1 className="font-bold">Terms & Conditions</h1>
           <br />
           <p>Effective Date: {new Date().toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' })}</p>
           <br />
