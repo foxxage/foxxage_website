de96ea4dcc0efd16ad7464f70e02723ee7bbdcc1
Foxxage <manage.foxxage@gmail.com>
2025-09-29 09:01:55 +0000

    ive one line space at bottom (_for element p_)

--- src/app/privacy-policy/page.tsx
+++ src/app/privacy-policy/page.tsx
@@ -10,7 +10,7 @@ export default function PrivacyPolicyPage() {
           <h1>Privacy Policy</h1>
           <br />
           <p>Effective Date: {new Date().toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' })}</p>
-          
+          <br />
           <p>At Foxxage, your privacy matters to us. This Privacy Policy explains what data we collect, how we use it, and how we keep it safe when you visit our website or use our services.</p>
 
           <br />
