b9156c6658bbe9e67c8d408368ffa244f411c9be
Foxxage <manage.foxxage@gmail.com>
2025-10-02 18:48:36 +0000

    Create Instagram, WhatsApp, Telegram, Google Sheet, YouTube, Gmail, Goog

--- src/components/sections/integrations-section.tsx
+++ src/components/sections/integrations-section.tsx
@@ -1,26 +1,23 @@
 
-import { Mail, Instagram, MessageCircle, Bot, Sheet, Trello } from 'lucide-react';
+import { Mail, Instagram, MessageCircle, Bot, Sheet } from 'lucide-react';
 import type { SVGProps } from 'react';
 
-const Hubspot = (props: SVGProps<SVGSVGElement>) => <svg {...props} viewBox="0 0 256 256"><path fill="currentColor" d="M128 0c70.7 0 128 57.3 128 128s-57.3 128-128 128S0 198.7 0 128 57.3 0 128 0z"></path><path fill="currentColor" d="M138.8 111.3c2.9-2.9 2.9-9.8 0-12.7a8.9 8.9 0 00-12.7 0l-33.5 33.5c-3 2.9-3 9.8 0 12.7a9 9 0 0012.7 0l33.5-33.5zM128.5 70.3a57.4 57.4 0 00-47.3 26.3c-2.4 3.7-1.4 8.7 2.3 11.1l11 7c3.7 2.4 8.7 1.4 11-2.3a29.8 29.8 0 0147.2 29.9c-.8 4.1 1.7 8.2 5.6 9.7l12.4 4.7c4 .8 8.1-1.7 9.6-5.6a57.4 57.4 0 00-52.8-80.8zm-19.1 116.3a29.9 29.9 0 01-47.2-29.8c.8-4.1-1.7-8.2-5.6-9.7l-12.4-4.7c-4-.8-8.1 1.7-9.6 5.6a57.4 57.4 0 0052.8 80.8c3-1.6 4.9-4.8 4.9-8.3v-11.8c0-4.9-3.4-9-8-10.1a5 5 0 01-4.9-2z"></path></svg>;
-const Slack = (props: SVGProps<SVGSVGElement>) => <svg {...props} viewBox="0 0 128 128"><path fill="currentColor" d="M51.6 62.9c0 4.1-3.3 7.4-7.4 7.4s-7.4-3.3-7.4-7.4 3.3-7.4 7.4-7.4h7.4v7.4z"></path><path fill="currentColor" d="M65.1 51.6c-4.1 0-7.4-3.3-7.4-7.4s3.3-7.4 7.4-7.4 7.4 3.3 7.4 7.4v7.4h-7.4z"></path><path fill="currentColor" d="M76.4 65.1c0-4.1 3.3-7.4 7.4-7.4s7.4 3.3 7.4 7.4-3.3 7.4-7.4 7.4h-7.4v-7.4z"></path><path fill="currentColor" d="M62.9 76.4c4.1 0 7.4 3.3 7.4 7.4s-3.3 7.4-7.4 7.4-7.4-3.3-7.4-7.4v-7.4h7.4z"></path><path fill="currentColor" d="M76.4 51.6c0-4.1 3.3-7.4 7.4-7.4h15.5c4.1 0 7.4 3.3 7.4 7.4s-3.3 7.4-7.4 7.4H83.7c-4.1 0-7.4-3.3-7.4-7.4z"></path><path fill="currentColor" d="M65.1 76.4c-4.1 0-7.4 3.3-7.4 7.4v15.5c0 4.1 3.3 7.4 7.4 7.4s7.4-3.3 7.4-7.4V83.7c0-4-3.3-7.3-7.4-7.3z"></path><path fill="currentColor" d="M51.6 76.4c0 4.1-3.3 7.4-7.4 7.4H28.7c-4.1 0-7.4-3.3-7.4-7.4s3.3-7.4 7.4-7.4h15.5c4.1 0 7.4 3.3 7.4 7.4z"></path><path fill="currentColor" d="M62.9 65.1c4.1 0 7.4-3.3 7.4-7.4V42.2c0-4.1-3.3-7.4-7.4-7.4s-7.4 3.3-7.4 7.4v15.5c0 4.1 3.3 7.4 7.4 7.4z"></path></svg>;
-
 const GoogleDrive = (props: SVGProps<SVGSVGElement>) => <svg {...props} viewBox="0 0 24 24"><path fill="currentColor" d="M7.71 3.5L1.5 14.24l2.85 4.93l6.21-10.75zM15.42 20.5l-2.86-4.94L6.35 20.5h9.07zM9.69 5.25L13.15 11l-4.59 7.95h9.18L22.5 11z"></path></svg>;
-const Stripe = (props: SVGProps<SVGSVGElement>) => <svg {...props} viewBox="0 0 24 24"><path fill="currentColor" d="M19.33 4.67a9.33 9.33 0 0 0-14.66 0H0v4.66h2.67a9.33 9.33 0 0 0 18.66 0H24V4.67h-4.67zm-9.33 10a4.67 4.67 0 1 1 0-9.34a4.67 4.67 0 0 1 0 9.34z"></path><path fill="currentColor" d="M0 14h24v4.67H0z"></path></svg>;
 const Gemini = (props: SVGProps<SVGSVGElement>) => <svg {...props} viewBox="0 0 24 24" fill="none"><path d="M5.104 2.103a.75.75 0 0 0-1.06 1.06l2.396 2.396a.25.25 0 0 1-.176.425H2.75a.75.75 0 0 0 0 1.5h3.51a.25.25 0 0 1 .176.424L4.045 10.3a.75.75 0 0 0 1.06 1.06l2.397-2.396a.25.25 0 0 1 .425.176v3.51a.75.75 0 0 0 1.5 0v-3.51a.25.25 0 0 1 .425-.176l2.396 2.396a.75.75 0 0 0 1.06-1.06L10.3 7.485a.25.25 0 0 1-.176-.424H13.6a.75.75 0 0 0 0-1.5h-3.47a.25.25 0 0 1-.176-.425l2.396-2.396a.75.75 0 0 0-1.06-1.06L8.892 4.045a.25.25 0 0 1-.425-.176V.36a.75.75 0 0 0-1.5 0v3.51a.25.25 0 0 1-.424.176L5.104 2.103z" fill="currentColor"></path><path d="M18.896 11.603a.75.75 0 0 0-1.06-1.06l-2.396 2.396a.25.25 0 0 1-.425-.176v-3.51a.75.75 0 0 0-1.5 0v3.51a.25.25 0 0 1-.425.176l-2.396-2.396a.75.75 0 0 0-1.06 1.06l2.396 2.396a.25.25 0 0 1-.176.425H9.4a.75.75 0 0 0 0 1.5h3.51a.25.25 0 0 1 .176.424l-2.396 2.397a.75.75 0 0 0 1.06 1.06l2.396-2.396a.25.25 0 0 1 .425.176v3.51a.75.75 0 0 0 1.5 0v-3.51a.25.25 0 0 1 .425-.176l2.396 2.396a.75.75 0 0 0 1.06-1.06l-2.396-2.397a.25.25 0 0 1 .176-.424h3.47a.75.75 0 0 0 0-1.5h-3.47a.25.25 0 0 1-.176-.425l2.396-2.396z" fill="currentColor"></path></svg>;
+const Telegram = (props: SVGProps<SVGSVGElement>) => <svg {...props} viewBox="0 0 24 24"><path fill="currentColor" d="M9.78 18.65l.28-4.23l7.68-6.92c.34-.31-.07-.46-.52-.19L7.74 13.3L3.64 12c-.88-.25-.89-.86.2-1.3l15.97-6.16c.73-.33 1.43.18 1.15 1.3l-2.72 12.58c-.28 1.13-1.04 1.4-1.74.88l-4.98-3.65l-2.33 2.22c-.24.24-.45.46-.8.46z" /></svg>;
+const Youtube = (props: SVGProps<SVGSVGElement>) => <svg {...props} viewBox="0 0 24 24"><path fill="currentColor" d="M10 15l5-3l-5-3v6m11.56-7.83c.13.47.22 1.1.28 1.9c.07.8.1 1.49.1 2.09L22 12c0 2.19-.16 3.8-.44 5.17c-.28 1.37-.9 2.3-1.82 2.83c-1.12.62-3.15.94-5.74.94s-4.62-.32-5.74-.94c-.92-.53-1.54-1.46-1.82-2.83c-.28-1.37-.44-2.98-.44-5.17s.16-3.8.44-5.17c.28-1.37.9-2.3 1.82-2.83c1.12-.62 3.15-.94 5.74-.94c2.59 0 4.62.32 5.74.94c.92.53 1.54 1.46 1.82 2.83Z" /></svg>;
+
 
 const logos = [
-    { icon: MessageCircle, name: 'WhatsApp' },
     { icon: Instagram, name: 'Instagram' },
+    { icon: MessageCircle, name: 'WhatsApp' },
+    { icon: Telegram, name: 'Telegram' },
+    { icon: Sheet, name: 'Google Sheet' },
+    { icon: Youtube, name: 'YouTube' },
+    { icon: Mail, name: 'Gmail' },
+    { icon: GoogleDrive, name: 'Google Drive' },
     { icon: Bot, name: 'ChatGPT' },
     { icon: Gemini, name: 'Gemini' },
-    { icon: Mail, name: 'Gmail' },
-    { icon: Sheet, name: 'Excel' },
-    { icon: Slack, name: 'Slack' },
-    { icon: Hubspot, name: 'Hubspot' },
-    { icon: Trello, name: 'Trello' },
-    { icon: GoogleDrive, name: 'Google Drive' },
-    { icon: Stripe, name: 'Stripe' },
 ];
 
 export function IntegrationsSection() {
